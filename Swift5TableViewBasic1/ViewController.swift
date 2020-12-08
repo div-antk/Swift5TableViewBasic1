2870cdbac3ada65977e97d15a117b3c4563dd882
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-08 22:36:11 +0900

    キーボードの処理を追加

--- Swift5TableViewBasic1/ViewController.swift
+++ Swift5TableViewBasic1/ViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
   
   @IBOutlet weak var textField: UITextField!
-  
   @IBOutlet weak var tableView: UITableView!
+  
+  var textArray = [String]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
 
     tableView.delegate = self
     tableView.dataSource = self
-    
-    
-    
-    
+    textField.delegate = self
+
   }
   
   
@@ -29,12 +28,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   
   // セクションの中のセルの数（必須）
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     <#code#>
   }
   
+  
   // セルの中身（必須）
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    <#code#>
+    return cell
+  }
+  
+  // セルの高さ
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return view.frame.size.height/6
+  }
+  
+  // キーボードのリターンが押されたときに発火
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    textArray.append(textField.text!)
+    // キーボードを閉じる
+    textField.resignFirstResponder()
+    textField.text = ""
+    tableView.reloadData()
+    
+    return true
   }
 }
 
